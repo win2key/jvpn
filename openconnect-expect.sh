ec7a5d3be3820948e0a768d974afd10b0637e2fa
lion <win2key@gmail.com>
2023-08-08 23:46:59 +0300

    Handle termination signals

--- openconnect-expect.sh
+++ openconnect-expect.sh
@@ -5,6 +5,13 @@ set user     $env(VPN_LOGIN)
 set password $env(VPN_PASSWORD)
 set server   $env(VPN_SERVER)
 
+# Handle termination signals
+trap {
+    send_user "\nCaught signal, terminating openconnect and exiting...\n"
+    send "\003" ;# Ctrl+C to terminate the openconnect session
+    exit
+} SIGINT SIGTERM
+
 spawn openconnect --protocol=AnyConnect --user=$user $server
 
 expect "Password:"
